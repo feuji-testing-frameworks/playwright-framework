d705ca49ebb3f74989d69e31646f839ac654e1b2
shirishamannem1010 <shirisha7143@gmail.com>
2024-05-22 09:55:48 +0530

    api changes

--- tests/ui_tests.spec.js
+++ tests/ui_tests.spec.js
@@ -1,23 +1,61 @@
+
 const { test, expect } = require('@playwright/test')
 require('dotenv').config();
 const indexPage = require('../pages/ui_pages/indexpage')
 const data = require('../data/ui_data.json')
 
-var loginPage
+var loginPage,homePage,checkoutPage,sideBarValidation
 test.beforeEach("Launch URL" , async ({ page }) => {
     loginPage = new indexPage.LoginPage(test,page);
+    homePage = new indexPage.HomePage(test,page);
+    checkoutPage = new indexPage.CheckoutPage(test,page);
+    sideBarValidation = new indexPage.SideBarValidation(test,page)
     await loginPage.launchUrl(process.env.baseUrl);
+    await page.waitForTimeout(parseInt(process.env.small_timeout));
 })
 
 test("Testing the login functionality with Valid credentials" , async ({ page }) => {
     await loginPage.loginFunctionality(process.env.standarduser,process.env.password);
-    await page.waitForTimeout(parseInt(process.env.medium_timeout));
     await expect(await loginPage.appTitle).toHaveText(data.applogoText);
+})
+
+test("Testing the login functionality with locked user and password" , async ({ page }) => {
+    await loginPage.loginFunctionality(process.env.lockedUser,process.env.password);
+    await expect(await loginPage.errorMsg).toBeVisible();
 })
 
 test("Testing the logout functionality" , async ({ page }) => {
     await loginPage.loginFunctionality(process.env.standarduser,process.env.password);
-    await page.waitForTimeout(parseInt(process.env.medium_timeout));
     await expect(await loginPage.appTitle).toHaveText(data.applogoText);
+    await loginPage.logoutFunctionality();
+})
+
+test("Testing the add to cart functionality" , async ({ page }) => {
+    await loginPage.loginFunctionality(process.env.standarduser,process.env.password);
+    await expect(await loginPage.appTitle).toHaveText(data.applogoText);
+    await homePage.addProductsToCart();
+    await expect(parseInt(await homePage.cartCount.textContent())).toBeGreaterThan(0);
+    await loginPage.logoutFunctionality();
+})
+
+test("Testing the checkout functionality", async({ page }) => {
+    await loginPage.loginFunctionality(process.env.standarduser,process.env.password);
+    await expect(await loginPage.appTitle).toHaveText(data.applogoText);
+    await homePage.addProductsToCart();
+    await checkoutPage.checkoutFunctionality(data.firstname,data.lastname,data.postalcode);
+    await expect(await checkoutPage.orderConfirmationMsg).toHaveText(data.orderConfirmationMsg);
+    await checkoutPage.clickOnBackToHomeButton();
+    await loginPage.logoutFunctionality();
+})
+
+test("Testing the elements present in the side bar" , async ({ page }) => {
+    await loginPage.loginFunctionality(process.env.standarduser,process.env.password);
+    await expect(await loginPage.appTitle).toHaveText(data.applogoText);
+    await sideBarValidation.clickOnSideBarButton();
+    for (var element of data.sideBarIds) {
+        await page.waitForTimeout(parseInt(process.env.small_timeout))
+        await expect(sideBarValidation.sideBarElement(element)).toBeVisible();
+    }
+    await sideBarValidation.clickOnCloseButton();
     await loginPage.logoutFunctionality();
 })
